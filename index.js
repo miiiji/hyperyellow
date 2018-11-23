bac9642f8a098f6c755e17afd40f9e80a95bc298
miiiji <alwldmsrk@gmail.com>
2018-11-23 11:35:32 +0900

    miji done

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
-    borderColor: 'yellow',
-    cursorColor: 'yellow',
+    borderColor: 'green',
+    cursorColor: 'green',
     css: `
       ${config.css || ''}
       .tabs_nav .tabs_list .tab_text {
-        color: yellow;
+        color: green;
       }
       .tabs_nav .tabs_title {
-        color: yellow;
+        color: green;
       }
     `
   });
